91438f8857c0f133f8f500fb9d6986d9f1be0fed
luisluft <luberluft@gmail.com>
2021-08-26 11:01:40 -0400

    add fullscreen button features

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const player = document.querySelector(".player");
 const speed = document.querySelector(".player-speed");
 const video = document.querySelector("video");
 const progressRange = document.querySelector(".progress-range");
@@ -94,6 +95,44 @@ function changeSpeed() {
 }
 
 // Fullscreen ------------------------------- //
+var elementToFullScreen = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen(elementToFullScreen) {
+  if (elementToFullScreen.requestFullscreen) {
+    elementToFullScreen.requestFullscreen();
+  } else if (elementToFullScreen.webkitRequestFullscreen) {
+    /* Safari */
+    elementToFullScreen.webkitRequestFullscreen();
+  } else if (elementToFullScreen.msRequestFullscreen) {
+    /* IE11 */
+    elementToFullScreen.msRequestFullscreen();
+  }
+  video.classList.add("video-fullscreen");
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    /* IE11 */
+    document.msExitFullscreen();
+  }
+  video.classList.remove("video-fullscreen");
+}
+
+let fullscreen = false;
+
+function toggleFullscreen() {
+  if (!fullscreen) openFullscreen(player);
+  else closeFullscreen();
+
+  fullscreen = !fullscreen;
+}
 
 // Event listeners
 playButton.addEventListener("click", togglePlay);
@@ -105,3 +144,4 @@ progressRange.addEventListener("click", setProgress);
 volumeRange.addEventListener("click", changeVolume);
 volumeIcon.addEventListener("click", toggleMute);
 speed.addEventListener("change", changeSpeed);
+fullscreenButton.addEventListener("click", toggleFullscreen);
